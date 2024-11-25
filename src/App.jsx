da7b70ed66b3ba21d7ceaf03a6d4ce96b17c66c4
taufan.zamzami <taufan.zamzami@jamiiis.local>
2024-11-25 19:22:29 +0700

    template pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
+import HomePage from './components/HomePage';
+import
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
